1b7195292f2cb7c71f0f16233655cd63c06ea8d9
Lobsie <gijs.duyck@gmail.com>
2023-03-14 21:28:07 +0100

    Add console app

---
+++ SixLetterWordChallenge/SixLetterWordChallenge.Cons/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SixLetterWordChallenge.Cons
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
