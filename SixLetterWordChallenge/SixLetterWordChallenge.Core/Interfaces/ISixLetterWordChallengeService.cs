8b28713e8af13bf2da531b3df90ee606119bc986
Lobsie <gijs.duyck@gmail.com>
2023-03-14 21:47:00 +0100

    Add interface and service in core project

---
+++ SixLetterWordChallenge/SixLetterWordChallenge.Core/Interfaces/ISixLetterWordChallengeService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SixLetterWordChallenge.Core.Interfaces
+{
+    public interface ISixLetterWordChallengeService
+    {
+        IList<string> GetInitialWords(string path);
+    }
+}
---
+++ SixLetterWordChallenge/SixLetterWordChallenge.Core/Services/SixLetterWordChallengeService.cs
@@ -0,0 +1,17 @@
+﻿using SixLetterWordChallenge.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SixLetterWordChallenge.Core.Services
+{
+    internal class SixLetterWordChallengeService : ISixLetterWordChallengeService
+    {
+        public IList<string> GetInitialWords(string path)
+        {
+            return File.ReadLines(path).Distinct().ToList();
+        }
+    }
+}
