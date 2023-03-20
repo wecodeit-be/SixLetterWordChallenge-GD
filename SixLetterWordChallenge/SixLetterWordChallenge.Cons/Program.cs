21fb50a5f05909659cd2c431d5ff026f0b50da36
Robert Van Haecke <robert.van.haecke@gmail.com>
2023-03-20 22:12:37 +0100

    Add comments to Program

--- SixLetterWordChallenge/SixLetterWordChallenge.Cons/Program.cs
+++ SixLetterWordChallenge/SixLetterWordChallenge.Cons/Program.cs
@@ -22,8 +22,12 @@ namespace SixLetterWordChallenge.Cons
 
             var words = sixLetterWordChallengeService.GetInitialWords(path + input).ToList();
 
+            // TODO: Why is this here? It should never be empty. Copy input.txt when building. (Build action)
             if (!words.Any())
             {
+                // TODO: Let's ignore the fact that this should not be here for a second.
+                // Following constructs should be avoided: Parent?.Parent?.Parent?.Parent?.FullName
+                // It's not super easy to read what this does.
                 path = Directory.GetParent(Directory.GetCurrentDirectory())?.Parent?.Parent?.Parent?.Parent?.FullName;
                 words = sixLetterWordChallengeService.GetInitialWords(path + input).ToList();
             }
@@ -33,6 +37,8 @@ namespace SixLetterWordChallenge.Cons
             foreach (var challengeword in challengeWords)
             {
                 Console.WriteLine(challengeword.ToPrint);
+                
+                // TODO: This list has no purpose. See comment on line 49
                 challengeWordsToPrint.Add(challengeword.ToPrint);
             }
 
@@ -40,6 +46,7 @@ namespace SixLetterWordChallenge.Cons
 
             string fileName = Console.ReadLine();
 
+            // Use LINQ instead of a new list
             File.WriteAllLines(path + "/" + fileName + ".txt", challengeWordsToPrint);
         }
     }
