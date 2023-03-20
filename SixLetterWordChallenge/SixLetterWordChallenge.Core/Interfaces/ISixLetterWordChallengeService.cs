4406d689daa4f869553086bc2ac44d2f3e5ee326
Robert Van Haecke <robert.van.haecke@gmail.com>
2023-03-20 22:15:40 +0100

    Add comment to ISixLetterWordChallengeService

--- SixLetterWordChallenge/SixLetterWordChallenge.Core/Interfaces/ISixLetterWordChallengeService.cs
+++ SixLetterWordChallenge/SixLetterWordChallenge.Core/Interfaces/ISixLetterWordChallengeService.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 
 namespace SixLetterWordChallenge.Core.Interfaces
 {
+    // TODO: The name of this interface doesn't mean anything. What does this interface stand for? 
+    // Example: IWordCombinationService, this tells me that it's used to make combinations with words.
     public interface ISixLetterWordChallengeService
     {
         IList<string> GetInitialWords(string path);
