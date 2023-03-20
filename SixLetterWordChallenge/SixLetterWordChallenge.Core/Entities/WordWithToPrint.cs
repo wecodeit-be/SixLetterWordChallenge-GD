12eecca707fbca212013ac51277f5006c4c53dfa
Robert Van Haecke <robert.van.haecke@gmail.com>
2023-03-20 22:18:05 +0100

    Add comment to WordWithToPrint

--- SixLetterWordChallenge/SixLetterWordChallenge.Core/Entities/WordWithToPrint.cs
+++ SixLetterWordChallenge/SixLetterWordChallenge.Core/Entities/WordWithToPrint.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace SixLetterWordChallenge.Core.Entities
 {
+    // TODO: Confusing name. Combining properties as a class name doesn't really mean anything.
     public class WordWithToPrint
     {
         public string Word { get; set; }
